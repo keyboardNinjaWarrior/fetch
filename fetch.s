530146d75228dbd202d44182341155906d3dba78
Nadeem Anwar <ibnul.aftab@proton.me>
2025-12-28 17:17:27 +0500

    - made a simple hello world program

--- fetch.s
+++ fetch.s
@@ -1,14 +1,23 @@
 	.file		"fetch"
 	.globl		_start
-	.section	.text
 
+	.section	.data
+string:
+	.asciz "Hello World!"
+
+	.section	.text
 _start:
 	// write function starts here
-
+write:
+	mov	x0,	#1
+	ldr	x1,	=string
+	mov	x2,	#12
+	mov	x8,	#64
+	svc	#0
 
 	// exit function starts here
 exit:
-	mov	x0,	#0		// return value in x0
+	mov	x0,	#0		// first parameter in x0
 	mov	x8,	#93		// x5D is will be placed in x8
 	svc	#0			// calling syscall
 	

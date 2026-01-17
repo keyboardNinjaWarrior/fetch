f5faef69953d075eff076dcc60e98330f14a8ba7
Nadeem Anwar <ibnul.aftab@proton.me>
2026-01-17 11:47:15 +0500

    - made progress     able to store data in sections

--- mem.s
+++ mem.s
@@ -1,7 +1,7 @@
 	.file		"mem"
 	.global		_start
 
-.section	.bss				// skipping space for variables
+	.section	.bss			// skipping space for variables
 cur_brk:					// current address of page break
 	.skip	8				// reserving 8 bytes
 end_brk:					// end of address of page break
@@ -18,7 +18,10 @@ end_brk:					// end of address of page break
 	.text
 _start:
 	_brk	#0				// get inital address
-
+	ldr	x1,	=cur_brk		// stores the the address of cur_brk in x1
+						// the '='Rn is used to obtain address here
+	str	x0,	[x1]			// stores the value in x0 at the address present in x1
+						// [Rn] = drefrencing Rn
 exit:
 	mov 	x8,	#0x5D
 	svc		#0

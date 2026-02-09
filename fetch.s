a310bbf745b01566aa98308a0b16b444eca66bd3
Nadeem Anwar <ibnul.aftab@proton.me>
2026-02-09 18:46:51 +0500

    - made problem much simpler - working on file handling

--- fetch.s
+++ fetch.s
@@ -1,33 +1,34 @@
 	.file		"fetch"
 	.globl		_start
+	.include	"exit.s"
 
 	.section	.data
-string:
+file:
 	.asciz		"lain.txt"
-
+read:
+	.space		1
+	
 	.section	.text
 _start:
-	// open function starts here
-open:
+	// openat	(int dfd, 
+	//			const char* filename, 
+	//			int flags, 
+	//			umode_tmode)
+
 	mov	x0,	#-100		// fd: -100 is magic number that tells to look into current directory
-	ldr	x1,	=string		// file name
+	ldr	x1,	=file		// file name
 	mov	x2,	#0			// flag: 0 stands for reading
 	mov	x3,	#0			// mode: 0 stands for null
 	mov	x8,	#0x38		// syscall: 0x38 for openat
 	svc	#0
-	b	exit
+	
+	// read		(unsigned int fd,
+	//			char* buf,
+	//			size_t count)
 
-	// write function starts here
-write:
-	mov	x0,	#1		// 1 is file discriptor for stdout
-	ldr	x1,	=string		// second parameter: address of the fist character of the string
-					// = is used to load the address
-	mov	x2,	#8		// third paramater: length of the string
-	mov	x8,	#64		// x40 is number for write syscall;
-	svc	#0			// should be placed in x8
+	ldr x1, =read
+	mov	x2, #1
+	mov	x8,	#0x3F
+	svc	#0
 
-	// exit function starts here
-exit:
-	mov	x0,	#0		// first parameter in x0
-	mov	x8,	#93		// x5D is will be placed in x8
-	svc	#0			// calling syscall	
+	_exit
--- makefile
+++ makefile
@@ -2,7 +2,7 @@ fetch: fetch.o
 	ld -o fetch fetch.o
 
 fetch.o: fetch.s
-	as -o fetch.o fetch.s
+	as --gstabs -o fetch.o fetch.s
 
 mem: mem.o
 	ld -o mem mem.o

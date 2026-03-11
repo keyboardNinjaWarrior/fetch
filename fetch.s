f21eb4660d79f73e05126c6aec21c115e2323294
Nadeem Anwar <ibnul.aftab@proton.me>
2026-03-11 20:29:58 +0500

    Can open file called lain.sixel

---
+++ fetch.s
@@ -0,0 +1,26 @@
+.file		"fetch.s"
+.include	"exit.s"
+.global		_start
+
+.section	.data
+file:		
+	.asciz	"lain.sixel"
+
+.section	.text
+_start:
+	mov	fp,	sp		// moving stack pointer into frame pointer for stack managment
+	
+	// opening file lain.sixel
+	
+	/* Using syscall openat */
+	/* https://www.man7.org/linux/man-pages/man3/openat.3p.html */
+	mov	w0,	-100		// -100 magic number for searching in te current directory
+	ldr	w1,	=file		// loading the string
+	mov	w2,	0		// 0000 for readonly
+	mov	w8,	0x38		// syscall for openat
+	svc	0
+	
+	sub	sp,	sp,	16
+	str	w0,	[sp]
+
+	exit	0

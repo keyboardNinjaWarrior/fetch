627aaef2da8b76c9a0dcd311ffefd2a7a1d92574
Nadeem Anwar <ibnul.aftab@proton.me>
2026-03-11 21:12:09 +0500

    Reading bytes from lain.sixel

--- fetch.s
+++ fetch.s
@@ -14,13 +14,23 @@ _start:
 	
 	/* Using syscall openat */
 	/* https://www.man7.org/linux/man-pages/man3/openat.3p.html */
+	/* https://www.man7.org/linux/man-pages/man3/open.3p.html */
+
 	mov	w0,	-100		// -100 magic number for searching in te current directory
 	ldr	w1,	=file		// loading the string
 	mov	w2,	0		// 0000 for readonly
 	mov	w8,	0x38		// syscall for openat
 	svc	0
 	
-	sub	sp,	sp,	16
-	str	w0,	[sp]
+	sub	sp,	sp,	16	// making room on the stack
+	str	w0,	[sp,	12]	// stores the file discriptor (int) on the stack 
+	
 
-	exit	0
+	// reading bytes from file	
+	ldr	w0,	[sp,	12]
+	add	x1,	sp,	11
+	mov	w2,	1
+	mov	x8,	0x3F
+	svc	0	
+	
+	_exit
